acaf3c1aa5b66ae4a1ef9a8aa0ecfa47c9cc3975
Siddharth Singh <siddharth1850@gmail.com>
2022-12-14 11:46:12 -0800

    Complete setup of Sequilize and setup the DB

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 const express = require('express')
+const bodyparser = require('body-parser')
+
 const {Port} = require('./config/serverconfig')
+
 async function setUpServer (){
     const app = express()
+
+    app.use(bodyparser.json)
+    app.use(bodyparser.urlencoded({extended:true}))
+
     app.listen(Port,()=>{
-        console.log("server started at port"+Port)
+        console.log("server started at port "+Port)
     })
 }
 
